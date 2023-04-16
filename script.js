425755e339e75790d1974b667728358ef2f062b3
brenobreves <brenocsb@dcc.ufrj.br>
2023-04-16 17:30:20 -0300

    Se mantendo ativo na sala e tratando as mensagens de status

--- script.js
+++ script.js
@@ -19,17 +19,33 @@ function loginOk(){
     const buton = document.querySelector('button');
     buton.disabled = false;
 }
-
+const notifyActive = setInterval(manterAtivo, 5000);
+function manterAtivo(){
+    let userObj = {name: `${username}`};
+    let reqStatus = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', userObj);
+}
+function reload() {
+    window.location.reload();
+}
 const pegamsg = axios.get("https://mock-api.driven.com.br/api/vm/uol/messages");
 pegamsg.then(exibeMsg);
 function exibeMsg(resp){
     const msgList = document.querySelector('.container');
     msgList.innerHTML = "";
     msgArr = resp.data;
-    console.log(msgArr[0])
+    console.log(msgArr[99]);
     for(let i = 0 ; i < 100 ; i++){
-    msgList.innerHTML += `<div class="msg"><span class="time">${msgArr[i].time}</span> <span class="fromto">${msgArr[i].from}</span> <span class="texto"> para </span> <span class="fromto">${msgArr[i].to}</span><span class="texto">: ${msgArr[i].text}</span></div>`;
+        if(msgArr[i].type == "status"){
+            msgList.innerHTML += `<div class="msg status" ><span class="time">${msgArr[i].time}</span> <span class="fromto">${msgArr[i].from}</span><span class="texto"> ${msgArr[i].text}</span></div>`;
+            continue
+        }
+        msgList.innerHTML += `<div class="msg"><span class="time">${msgArr[i].time}</span> <span class="fromto">${msgArr[i].from}</span> <span class="texto"> para </span> <span class="fromto">${msgArr[i].to}</span><span class="texto">: ${msgArr[i].text}</span></div>`;
     }
+}
+const attpag = setInterval(attMsg, 3000);
+function attMsg(){
+    const attmsg = axios.get("https://mock-api.driven.com.br/api/vm/uol/messages");
+    attmsg.then(exibeMsg);
 }
 function sendmsg(){
     alert("Oi");
